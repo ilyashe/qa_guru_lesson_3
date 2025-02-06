4f1e5c52589302801af451e32d8ca82a0f4b72d7
Ilia Shebanov <ilya.shebanoff@gmail.com>
2025-02-06 16:45:42 +0200

    add test_duckduckgo_search.py with test_duckduckgo_should_find_selene

---
+++ test_duckduckgo_search.py
@@ -0,0 +1,7 @@
+from selene import browser, be, have
+
+
+def test_duckduckgo_should_find_selene():
+    browser.open('https://duckduckgo.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="r1-0"]').should(have.text('Selene: User-Oriented Web UI Browser Tests in Python - GitHub Pages'))
