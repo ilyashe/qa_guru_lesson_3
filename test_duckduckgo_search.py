efa648be20f42904e604d92b4aca1c6fe788addc
Ilia Shebanov <ilya.shebanoff@gmail.com>
2025-02-06 17:59:08 +0200

    add test_duckduckgo_should_find_nothing

--- test_duckduckgo_search.py
+++ test_duckduckgo_search.py
@@ -2,5 +2,10 @@ from selene import browser, be, have
 
 
 def test_duckduckgo_should_find_selene(open_duckduckgo):
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="r1-0"]').should(have.text('Selene: User-Oriented Web UI Browser Tests in Python - GitHub Pages'))
+    browser.element('[name=q]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('#r1-0').should(have.text('Selene: User-Oriented Web UI Browser Tests in Python - GitHub Pages'))
+
+
+def test_duckduckgo_should_find_nothing(open_duckduckgo):
+    browser.element('[name=q]').should(be.blank).type('blorkznak').press_enter()
+    browser.element('#react-layout').should(have.text('результаты не найдены'))
