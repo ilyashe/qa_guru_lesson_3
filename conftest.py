b4cc6929dd8a0cf1e3d4511366a21beedd611ad2
Ilia Shebanov <ilya.shebanoff@gmail.com>
2025-02-06 22:05:49 +0200

    change fixture

--- conftest.py
+++ conftest.py
@@ -3,13 +3,9 @@ from selene import browser
 
 
 @pytest.fixture
-def open_duckduckgo(setting_browser):
-    browser.open('https://duckduckgo.com')
-    yield
-    browser.quit()
-
-
-@pytest.fixture
 def setting_browser():
     browser.config.window_width = 1200
     browser.config.window_height = 700
+    browser.open('https://duckduckgo.com')
+    yield
+    browser.quit()
--- test_duckduckgo_search.py
+++ test_duckduckgo_search.py
@@ -1,11 +1,11 @@
 from selene import browser, be, have
 
 
-def test_duckduckgo_should_find_selene(open_duckduckgo):
+def test_duckduckgo_should_find_selene(setting_browser):
     browser.element('[name=q]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('#r1-0').should(have.text('Selene: User-Oriented Web UI Browser Tests in Python - GitHub Pages'))
 
 
-def test_duckduckgo_should_find_nothing(open_duckduckgo):
+def test_duckduckgo_should_find_nothing(setting_browser):
     browser.element('[name=q]').should(be.blank).type('blorkznak').press_enter()
     browser.element('#react-layout').should(have.text('результаты не найдены'))
